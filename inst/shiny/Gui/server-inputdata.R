f5c9e860b17588923ea86d31b170dfc181be8ce5
Dong <yonghui.dong@gmail.com>
2021-05-11 23:18:13 +0300

    new example data
    add new data

--- inst/shiny/Gui/server-inputdata.R
+++ inst/shiny/Gui/server-inputdata.R
@@ -1,26 +1,26 @@
 #### load Data
 userInput <- reactive({
-  
+
   if(input$example_data == "yes") {
-    
-    if(input$example_dataset == "st000284"){
-      target <- CCWeights::noSTD
+
+    if(input$example_dataset == "std"){
+      target <- CCWeights::expData$STD
     } else{
-      target <- CCWeights::noSTD 
+      target <- CCWeights::expData$noSTD
     }
     return(target)
   }
-  
+
   else if (input$example_data == "umd") {
-    
+
     infile <- input$target
-    
+
     if (is.null(infile)){
       return(NULL)
     }
-    
+
     else {
-      
+
       extension <- tools::file_ext(infile$name)
       filepath <- infile$datapath
       target = switch(extension,
@@ -28,7 +28,7 @@ userInput <- reactive({
                       xls = readxl::read_xls(filepath),
                       xlsx = readxl::read_xlsx(filepath)
       )
-      
+
       # Assign the compound name if it is not specified
       if (is.null(target$Compound)) {target$Compound = "X"}
       return(as.data.frame(target))
@@ -38,35 +38,35 @@ userInput <- reactive({
 
 
 observeEvent(input$upload_data, {
-  
+
   output$rawData <- DT::renderDataTable({
-    
-    shiny::validate(need(all(c("Concentration", "Response") %in% colnames(userInput())), 
+
+    shiny::validate(need(all(c("Concentration", "Response") %in% colnames(userInput())),
                          "You data should contain at least 'Concentration' and 'Response' columns"))
 
     datatable(userInput(),
-              class = 'cell-border stripe', 
-              rownames = FALSE, 
-              filter = "top", 
+              class = 'cell-border stripe',
+              rownames = FALSE,
+              filter = "top",
               extensions = 'Buttons'
               )
-    })  
-  
+    })
+
   output$summaryData <- DT::renderDataTable({
-    
-    shiny::validate(need(all(c("Concentration", "Response") %in% colnames(userInput())), 
+
+    shiny::validate(need(all(c("Concentration", "Response") %in% colnames(userInput())),
                          "You data should contain at least 'Concentration' and 'Response' columns"))
-    
+
     # get summary
     dataSummary <- userInput() %>%
       dplyr::group_by(Compound, Concentration) %>%
       dplyr::summarise(Count = dplyr::n(), .groups = 'drop')
-    
+
     datatable(dataSummary,
               options = list(ordering = FALSE),
-              class = 'cell-border stripe', 
-              rownames = FALSE, 
-              extensions = 'Buttons', 
+              class = 'cell-border stripe',
+              rownames = FALSE,
+              extensions = 'Buttons',
               editable = FALSE
               )
     })
--- inst/shiny/Gui/ui-tab-inputdata.R
+++ inst/shiny/Gui/ui-tab-inputdata.R
@@ -1,7 +1,7 @@
 fluidRow(
-  
+
   column(width = 12,
-         
+
          bs4Card(
            width = 12,
            title = "Instruction",
@@ -11,7 +11,7 @@ fluidRow(
            collapsed = FALSE,
            closable = FALSE,
            p("1. You can upload your data in Data Upload tab. CCWeights accepts csv, xls and xlsx formats.'"),
-           p("2. Your data should contain at least two columns, i.e., 'concentration' and 'Response'. 
+           p("2. Your data should contain at least two columns, i.e., 'concentration' and 'Response'.
              If your data contains information from more than one metabolites, you need to add an additional column, named 'Compound'.
              If you have stable isotope labeled standards in your data, you need to add a column named 'IS'."),
            p("3. You can load the two example datasets in Data Upload tab to check the data format."),
@@ -19,10 +19,10 @@ fluidRow(
            p("5. You can click '+' and '-' in the tab to show or hide the contents in the tab.")
            )
          ),
-  
-  
+
+
   column(width = 3,
-         
+
          bs4Card(
            width = 12,
            inputId = "input_card",
@@ -38,10 +38,10 @@ fluidRow(
                         selected = 'yes'),
            conditionalPanel(condition = "input.example_data == 'yes'",
                             radioButtons("example_dataset", "Please choose an example dataset:",
-                                         choices = c("Example with internal standard" = 'st000284',
-                                                     "Example without internal standard" = 'st000336'
+                                         choices = c("Example with internal standard" = 'std',
+                                                     "Example without internal standard" = 'nostd'
                                                      ),
-                                         selected = 'st000284')
+                                         selected = 'std')
                             ),
            conditionalPanel(condition = "input.example_data == 'umd'",
                             fileInput("target","Upload your file (csv/xls/xlsx format):")
@@ -52,7 +52,7 @@ fluidRow(
            )
          ),
 
-  
+
   column(width = 9,
          bs4Card(
            width = 12,
@@ -65,7 +65,7 @@ fluidRow(
            closable = FALSE,
            DT::dataTableOutput("rawData")
            ),
-         
+
          bs4Card(
            width = 12,
            inputId = "summary_card",
