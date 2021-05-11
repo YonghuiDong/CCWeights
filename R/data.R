9d84f896effca7ddd86ad5c82c4206469e9e53cd
Dong <yonghui.dong@gmail.com>
2021-05-11 23:17:33 +0300

    add new example data

--- R/data.R
+++ R/data.R
@@ -1,11 +1,10 @@
-#' noSTD
+#' expData
 #'
-#' example data without stable isotope labeled standard
+#' Two example data set: one with internal standards (IS), and one without IS
 #'
-#' @format A data frame with 10000 rows and 2 variables:
+#' @format A list with 2 data frames:
 #' \describe{
-#'   \item{Compound}{Name of the compound}
-#'   \item{Concentration}{Concentration, use 'unknown' to indicate it is a sample type not standard}
-#'   \item{Response}{Response}
+#'   \item{noSTD}{the example data without IS}
+#'   \item{STD}{the example data with IS}
 #' }
-"noSTD"
+"expData"
