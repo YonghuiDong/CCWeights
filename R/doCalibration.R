6ffd7442bedc453c4afccb132ed32f87f346c22c
Dong <yonghui.dong@gmail.com>
2021-05-11 19:07:21 +0300

    Update doCalibration.R
    solve #2

--- R/doCalibration.R
+++ R/doCalibration.R
@@ -19,25 +19,34 @@ doCalibration <- function(DF, weights = NULL){
   DFA <- DF %>%
     dplyr::filter(Concentration == 'unknown')
 
-  # get ratio
-  if (is.null(DFA$IS)) {
-    DFA$Ratio = DFA$Response
-  } else {
-    DFA$Ratio = round(DFA$Response/DFA$IS, 3)
-  }
+  if(nrow(DFA) > 0){
 
-  # get model
-  model <-  if(inherits(try(doWlm(DF, weights = weights)$model, silent = TRUE), "try-error")){NULL} else {doWlm(DF, weights = weights)$model}
-
-  # get y
-  if (!is.null(model)){
-    for (i in 1:dim(DFA)[1]) {
-      DFA$Concentration[i] <- round(predict(model, data.frame(x = c(DFA$Ratio[i]))), 3)
+    # get ratio
+    if (is.null(DFA$IS)) {
+      DFA$Ratio = DFA$Response
+    } else {
+      DFA$Ratio = round(DFA$Response/DFA$IS, 3)
     }
+
+    # get model
+    model <-  if(inherits(try(doWlm(DF, weights = weights)$model, silent = TRUE), "try-error")){NULL} else {doWlm(DF, weights = weights)$model}
+
+    # get y
+    if (!is.null(model)){
+      for (i in 1:dim(DFA)[1]) {
+        DFA$Concentration[i] <- round(predict(model, data.frame(x = c(DFA$Ratio[i]))), 3)
+      }
+    }
+
+    if (is.null(weights)){weights = "1"}
+    calResult <- cbind.data.frame(DFA, Model = weights)
+
+  } else {
+
+    if (is.null(weights)){weights = "1"}
+    calResult <- cbind.data.frame(Sample = "No sample detected", Model = weights)
   }
 
-  if (is.null(weights)){weights = "1"}
-  calResult <- cbind.data.frame(DFA, Model = weights)
   return(calResult)
 }
 
