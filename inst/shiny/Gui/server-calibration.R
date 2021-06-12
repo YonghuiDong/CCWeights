883236393265c672ac3be4080bc40d8c64d32441
Dong <yonghui.dong@gmail.com>
2021-06-12 10:30:42 +0300

    typo

--- inst/shiny/Gui/server-calibration.R
+++ inst/shiny/Gui/server-calibration.R
@@ -50,7 +50,7 @@ observeEvent(input$caliDo, {
 
                           output$caliSave<- downloadHandler(
                             filename = function() {
-                              paste("quantificationResults", ".csv", sep="")
+                              paste("quantificationResultsƒ", ".csv", sep="")
                             },
                             content = function(file) {
                               write.csv(CaliFinal, file, row.names = FALSE)
